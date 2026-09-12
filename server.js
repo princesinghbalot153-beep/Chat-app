7fa4e71825cf9d70d05e708de7928135432ab3b1
princesinghbalot153-beep <princesinghbalot153@gmail.com>
2026-09-12 13:24:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // Online users: { socketId: { id, name } }
 const users = new Map();
