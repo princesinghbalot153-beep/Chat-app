afdd9e8b300e071daa757ac5aae5fb16f5eead40
princesinghbalot153-beep <princesinghbalot153@gmail.com>
2026-09-13 23:52:58 +0530

    Add logging for service worker registration
    Enhanced service worker registration with logging.

--- app.js
+++ app.js
@@ -364,8 +364,13 @@ function addMessage(msg) {
 }
 
 // Service worker
+
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js');
+  navigator.serviceWorker.register('/sw.js').then(reg => {
+    console.log('Service Worker registered:', reg);
+  }).catch(err => {
+    console.error('Service Worker error:', err);
+  });
 }
 
 socket.on('typing', ({ from, name }) => {
